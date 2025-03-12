0ac1753326089cb43097bf666f07fc857cf82fd6
golda <goldaatemba610@gmail.com>
2025-03-12 07:25:18 +0300

    added files

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+    // Use the find() method to locate the first game with a win ("W")
+    const win = record.find(game => game.result === "W");
+    
+    // Return the year of the win if found, otherwise return undefined
+    return win ? win.year : undefined;
+  }
+  
+  module.exports = superbowlWin;
+  
